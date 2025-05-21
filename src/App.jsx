76ee01e2d03613e69f4cbdf4d5b30f92a30437cf
iamadams <iamadams4523@gmail.com>
2025-05-21 07:44:02 +0100

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,24 @@ import GraphImg from './assets/ChatGPT Image Apr 7, 2025, 12_16_55 AM.png';
 import { Switch } from '@/components/ui/switch';
 import { FaLaptopCode } from 'react-icons/fa';
 import { FaCheckCircle } from 'react-icons/fa';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
 
+const fruitsItems = ['corn', 'Banana', 'Yam', 'Beans', 'Mango'];
+const drynessItems = ['Dry', 'Mildy Dry', 'Very Dry'];
 function App() {
   const [isDark, setIsDark] = useState(false);
 
@@ -57,7 +74,7 @@ function App() {
           <h3 className="font-[Inder] font-normal text-base ml-6">
             Live Data Overview
           </h3>
-          <div className="border border-1-grey-500 p-2 flex flex-col mt-3 rounded-2xl pl-5">
+          <div className="border border-gray-300 p-2 flex flex-col mt-3 rounded-2xl pl-5">
             <p className="font-[Inder] text-[14px] ">Temperature</p>
             <p className="font-[Inconsolata] font-extrabold text-[40px]">
               135°c
@@ -73,20 +90,73 @@ function App() {
           </p>
 
           <div className="mt-2 flex gap-4">
-            <div className="w-[30%] flex flex-col justify-center items-center border border-1-grey-500 rounded-xl p-2 ">
+            <div className="w-[30%] flex flex-col justify-center items-center border border-gray-300 rounded-xl p-2 ">
               <p className="font-[Inder] text-[14px]">Temperature & Humidity</p>
               <img className="w-[95%]" src={GraphImg} alt="" />
             </div>
 
-            <div className="w-[30%] flex flex-col justify-center items-center border border-1-grey-500 rounded-xl p-2 mt ">
+            <div className="w-[30%] flex flex-col justify-center items-center border border-gray-300 rounded-xl p-2 mt ">
               <p className="font-[Inder] text-[14px]">Temperature & Humidity</p>
               <img className="w-[95%]" src={GraphImg} alt="" />
             </div>
 
-            <div className="w-[30%] flex flex-col justify-center items-center border border-1-grey-500 rounded-xl p-2 mt ">
+            <div className="w-[30%] flex flex-col justify-center items-center border border-gray-300 rounded-xl p-2 mt ">
               <p className="font-[Inder] text-[14px]">Efficiency</p>
               <p className="border-6 border-blue-300 p-3 rounded-[50%]">92%</p>
             </div>
+          </div>
+
+          <div className="mt-3 w-[40%]">
+            <h3 className="font-[Inder] font-normal text-base ml-6">
+              Control Panel
+            </h3>
+
+            <DropdownMenu>
+              <DropdownMenuTrigger className="w-[80%] outline-none">
+                <p className="w-full font-[Inder] text-[16px] text-center mt-3 border border-gray-300 py-2 rounded-[12px]">
+                  Start Drying
+                </p>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel className="text-xl">
+                  Start Drying
+                </DropdownMenuLabel>
+                <div className="mt-3 flex flex-col gap-3 items-center">
+                  <h2 className="text-[13px] font-semibold ml-3 self-start">
+                    Drying Mode
+                  </h2>
+                  <Select>
+                    <SelectTrigger className="w-[160px]">
+                      <SelectValue placeholder="Fruits" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {fruitsItems.map((fruit, index) => (
+                        <SelectItem key={index} value={fruit}>
+                          {fruit}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                </div>
+                <div className="mt-3 flex flex-col gap-3 items-center">
+                  <h2 className="text-[13px] font-semibold ml-3 self-start">
+                    Dryness Level
+                  </h2>
+                  <Select>
+                    <SelectTrigger className="w-[160px]">
+                      <SelectValue placeholder="dryness level" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {drynessItems.map((fruit, index) => (
+                        <SelectItem key={index} value={fruit}>
+                          {fruit}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                </div>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
         </div>
       </div>
