ba4a21d544b8021b7bfa9086b5c5fbd6eac778a9
iamadams <iamadams4523@gmail.com>
2025-05-23 15:01:59 +0100

    Final UI design

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -20,6 +20,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select';
+import { Slider } from '@/components/ui/slider';
 
 const fruitsItems = ['corn', 'Banana', 'Yam', 'Beans', 'Mango'];
 const drynessItems = ['Dry', 'Mildy Dry', 'Very Dry'];
@@ -115,10 +116,10 @@ function Home() {
             </div>
             <div className="border border-gray-300 p-2 flex flex-col mt-3 rounded-2xl pl-5 w-[45%]">
               <p className="font-[Inder] text-[14px] font-semibold ">
-                Temperature
+                Humidity
               </p>
               <p className="font-[Inconsolata] font-extrabold text-[40px]">
-                135°c
+                22%
               </p>
               <div className="w-fit bg-green-100 rounded-[10px] px-6 py-0.5 flex items-center justify-center gap-3">
                 <FaCheckCircle className="text-green-700" />
@@ -148,65 +149,145 @@ function Home() {
             </div>
           </div>
 
-          <div className="mt-3 w-[40%]">
-            <h3 className="font-[Inder] font-normal text-base ml-6">
-              Control Panel
-            </h3>
+          <div className="flex items-center gap-5 mt-3">
+            <div className="w-[60%]">
+              <h3 className="font-[Inder] font-semibold text-base text-center">
+                Control Panel
+              </h3>
 
-            <DropdownMenu>
-              <DropdownMenuTrigger className="w-[80%] outline-none">
-                <p className="w-full font-[Inder] text-[16px] text-center mt-3 border border-gray-300 py-2 rounded-[12px]">
-                  Start Drying
-                </p>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuLabel className="text-xl">
-                  Start Drying
-                </DropdownMenuLabel>
-                <div className="mt-3 flex flex-col gap-3 items-center">
-                  <h2 className="text-[13px] font-semibold ml-3 self-start">
-                    Drying Mode
-                  </h2>
-                  <Select>
-                    <SelectTrigger className="w-[160px]">
-                      <SelectValue placeholder="Fruits" />
-                    </SelectTrigger>
-                    <SelectContent>
-                      {fruitsItems.map((fruit, index) => (
-                        <SelectItem key={index} value={fruit}>
-                          {fruit}
-                        </SelectItem>
-                      ))}
-                    </SelectContent>
-                  </Select>
-                </div>
-                <div className="mt-3 flex flex-col gap-3 items-center">
-                  <h2 className="text-[13px] font-semibold ml-3 self-start">
-                    Dryness Level
-                  </h2>
-                  <Select>
-                    <SelectTrigger className="w-[160px]">
-                      <SelectValue placeholder="dryness level" />
-                    </SelectTrigger>
-                    <SelectContent>
-                      {drynessItems.map((fruit, index) => (
-                        <SelectItem key={index} value={fruit}>
-                          {fruit}
-                        </SelectItem>
-                      ))}
-                    </SelectContent>
-                  </Select>
-                </div>
-                <div
-                  onClick={() => {
-                    setRunning(true);
-                  }}
-                  className="font-[inconsolata] font-bold text-sm text-white text-center px-1 py-1 bg-green-400 mt-7 rounded-[10px] cursor-pointer"
-                >
-                  Submit
-                </div>
-              </DropdownMenuContent>
-            </DropdownMenu>
+              <div className="flex w-full gap-3">
+                <DropdownMenu>
+                  <DropdownMenuTrigger className="w-[50%] outline-none">
+                    <p className="w-full font-[Inder] text-[16px] text-center mt-3 border border-gray-300 py-2 rounded-[12px]">
+                      Start Drying
+                    </p>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    <DropdownMenuLabel className="text-xl">
+                      Start Drying
+                    </DropdownMenuLabel>
+                    <div className="mt-3 flex flex-col gap-3 items-center">
+                      <h2 className="text-[13px] font-semibold ml-3 self-start">
+                        Drying Mode
+                      </h2>
+                      <Select>
+                        <SelectTrigger className="w-[160px]">
+                          <SelectValue placeholder="Fruits" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          {fruitsItems.map((fruit, index) => (
+                            <SelectItem key={index} value={fruit}>
+                              {fruit}
+                            </SelectItem>
+                          ))}
+                        </SelectContent>
+                      </Select>
+                    </div>
+                    <div className="mt-3 flex flex-col gap-3 items-center">
+                      <h2 className="text-[13px] font-semibold ml-3 self-start">
+                        Dryness Level
+                      </h2>
+                      <Select>
+                        <SelectTrigger className="w-[160px]">
+                          <SelectValue placeholder="dryness level" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          {drynessItems.map((fruit, index) => (
+                            <SelectItem key={index} value={fruit}>
+                              {fruit}
+                            </SelectItem>
+                          ))}
+                        </SelectContent>
+                      </Select>
+                    </div>
+                    <div
+                      onClick={() => {
+                        setRunning(true);
+                      }}
+                      className="font-[inconsolata] font-bold text-sm text-white text-center px-1 py-1 bg-green-400 mt-7 rounded-[10px] cursor-pointer"
+                    >
+                      Submit
+                    </div>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+
+                <DropdownMenu>
+                  <DropdownMenuTrigger className="w-[50%] outline-none">
+                    <p className="w-full font-[Inder] text-[16px] text-center mt-3 border border-gray-300 py-2 rounded-[12px]">
+                      Custom Mode
+                    </p>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    <div className="flex flex-col justify-center items-center">
+                      <div className="flex flex-col">
+                        <label
+                          className="font-[inconsolata] font-bold text-sm"
+                          htmlFor="temperature"
+                        >
+                          Temperature
+                        </label>
+                        <input
+                          id="temperature"
+                          className="outline w-[7rem] h-[1.5rem] pl-3 bg-white rounded-[7px]"
+                          type="text"
+                        />
+                      </div>
+                    </div>
+                    <div className="flex flex-col gap-4 justify-center items-center">
+                      <div className="flex flex-col mt-5">
+                        <label
+                          className="font-[inconsolata] font-bold text-sm"
+                          htmlFor="humidity"
+                        >
+                          Humidity
+                        </label>
+                        <input
+                          id="humidity"
+                          className="outline w-[7rem] h-[1.5rem] pl-3 bg-white rounded-[7px]"
+                          type="text"
+                        />
+                      </div>
+                    </div>
+                    <div
+                      onClick={() => {
+                        setRunning(true);
+                      }}
+                      className="font-[inconsolata] font-bold text-sm text-white text-center px-1 py-1 bg-green-400 mt-7 rounded-[10px] cursor-pointer"
+                    >
+                      Submit
+                    </div>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </div>
+            </div>
+
+            <div className="mt-3 w-[40%]">
+              <h3 className="font-[Inder] font-semibold text-base ml-[4rem]">
+                Extra Status
+              </h3>
+
+              <DropdownMenu>
+                <DropdownMenuTrigger className="w-[80%] outline-none">
+                  <p className="w-full font-[Inder] text-[16px] text-center mt-3 border border-gray-300 py-2 rounded-[12px]">
+                    Fan and Heater status
+                  </p>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="h-[10rem] flex flex-col gap-5">
+                  <div className="mt-3 flex flex-col gap-3 items-center">
+                    <h2 className="text-[13px] font-semibold  self-start">
+                      Fan status
+                    </h2>
+                    <Slider defaultValue={[33]} max={100} step={1} />
+                  </div>
+                  <div className="mt-5  flex flex-col gap-3 items-center">
+                    <h2 className="text-[13px] font-semibold self-start">
+                      Heater status
+                    </h2>
+                    <Slider defaultValue={[33]} max={100} step={2} />
+                  </div>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
           </div>
         </div>
       </div>
